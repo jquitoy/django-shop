e6a9aee1d1607e556a7ac05688809ef1b680a8e0
joshuaquitoy22@gmail.com <joshuaquits@gmail.com>
2025-03-27 22:24:07 +0800

    Register New Users - Django Wednesdays ECommerce 8

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
 
 def home(request):
 	products = Product.objects.all()
@@ -31,3 +36,22 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, ("You have been logged out. Thanks for shopping!"))
 	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# login user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("You have been registered successfully. Welcome!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("Oops! There was a problem registering. Try again."))
+			return redirect('register')
+	else:
+		return render(request, 'register.html', {'form': form})
